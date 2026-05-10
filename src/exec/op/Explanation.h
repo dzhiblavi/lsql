7767522255ff229c4c944d26a2888d2bd0548199
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2026-05-10 20:26:14 +0300

    Split Explanation into a separate file

---
+++ src/exec/op/Explanation.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "exec/op/Subscriber.h"
+
+#include <sstream>
+
+namespace lsql::exec {
+
+class Operation;
+
+class ExplanationItem {
+ public:
+    ExplanationItem() = default;
+
+    bool empty() const { return lines.empty(); }
+
+    template <typename... Args>
+    ExplanationItem& line(std::format_string<const Args&...> fmt, const Args&... args) {
+        lines.push_back(std::format(fmt, args...));
+        return *this;
+    }
+
+    ExplanationItem& child(ExplanationItem explanation) {
+        for (auto&& line : explanation.lines) {
+            lines.push_back(std::format("  {}", line));
+        }
+        return *this;
+    }
+
+    ExplanationItem& block(ExplanationItem explanation) {
+        for (auto&& line : explanation.lines) {
+            lines.push_back(std::move(line));
+        }
+        return *this;
+    }
+
+    std::string format() const {
+        std::stringstream ss;
+        for (auto&& line : lines) {
+            ss << line << '\n';
+        }
+        return ss.str();
+    }
+
+ private:
+    std::vector<std::string> lines;
+};
+
+class Explanation {
+ public:
+    Explanation() = default;
+
+    void insert(ExplanationItem item, const Operation* op) {
+        if (item.empty()) {
+            return;
+        }
+
+        items.emplace(op, std::move(item));
+    }
+
+    std::string format() const {
+        std::stringstream ss;
+        for (auto&& [_, item] : items) {
+            ss << item.format() << '\n';
+        }
+        return ss.str();
+    }
+
+ private:
+    std::unordered_map<const Operation*, ExplanationItem> items;
+};
+
+struct ExplanationCtx {
+    const Subscriber* requester = nullptr;
+    const int phase = 0;
+    Explanation& explanation;
+
+    ExplanationCtx withRequester(const Subscriber* sub) const {
+        ExplanationCtx ctx{*this};
+        ctx.requester = sub;
+        return ctx;
+    }
+};
+
+}  // namespace lsql::exec
