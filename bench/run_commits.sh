624ade7b51b51fb58700db81c48bbc9bcb0933b6
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2026-06-14 10:46:07 +0300

    Change run_commits.sh

--- bench/run_commits.sh
+++ bench/run_commits.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo
 
+repeat="${1}"
+shift
+
 for commit in "$@"; do
     git checkout "${commit}"
-    python3 bench/run.py --build-type Release --repeat 50 --warmup 5
+    python3 bench/run.py --build-type Release --repeat "${repeat}" --warmup 5
 done
